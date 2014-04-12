fb9d7c048e9827aeade020518be8ba689bfb8be2
Tomi Paananen <tomi.paananen@nokia.com>
2014-04-12 13:57:50 +0300

    Initial commit.

---
+++ src/com/example/deeplinksample/MainActivity.java
@@ -0,0 +1,109 @@
+package com.example.deeplinksample;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.os.Build;
+import android.preference.PreferenceManager;
+
+public class MainActivity extends ActionBarActivity {
+    // Constants
+    private static final String MARKET_SCHEME_PREFIX = "market://";
+    private static final String STORE_WEB_URL_PREFIX = "http://store.ovi.mobi/content/";
+    private static final String MARKET_SCHEME_QUERY_STRING_ID = "market_scheme_query_string";
+    private static final String STORE_WEB_URL_CONTENT_ID = "store_web_url_content_id";
+    private static final String DEFAULT_MARKET_SCHEME_QUERY_STRING = "details?id=com.nokia.app.mixradio.client";
+    private static final String DEFAULT_STORE_WEB_URL_CONTENT_ID = "488076";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .add(R.id.container, new PlaceholderFragment()).commit();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        initView();
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d("DeepLinkSample/MainActivity", "onPause()");
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final EditText queryEditText = (EditText) findViewById(R.id.marketSchemeQueryEditText);
+        preferences.edit().putString(MARKET_SCHEME_QUERY_STRING_ID, queryEditText.getText().toString()).commit();
+        final EditText contentIdEditText = (EditText) findViewById(R.id.contentIdEditText);
+        preferences.edit().putString(STORE_WEB_URL_CONTENT_ID, contentIdEditText.getText().toString()).commit();
+        super.onPause();
+    }
+
+    private void launchMarketScheme(String queryString) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(MARKET_SCHEME_PREFIX + queryString));
+        startActivity(intent);
+    }
+
+    private void launchStoreWebUrl(String contentIdString) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(Uri.parse(STORE_WEB_URL_PREFIX + contentIdString));
+        startActivity(intent);
+    }
+
+    private void initView() {
+        final Button marketSchemeGoButton = (Button) findViewById(R.id.marketSchemeGoButton);
+        marketSchemeGoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final EditText queryEditText = (EditText) findViewById(R.id.marketSchemeQueryEditText);
+                launchMarketScheme(queryEditText.getText().toString());
+            }
+        });
+        
+        final Button storeWebUrlGoButton = (Button) findViewById(R.id.storeWebUrlGoButton);
+        storeWebUrlGoButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                final EditText contentIdEditText = (EditText) findViewById(R.id.contentIdEditText);
+                launchStoreWebUrl(contentIdEditText.getText().toString());
+            }
+        });
+        
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
+        final EditText queryEditText = (EditText) findViewById(R.id.marketSchemeQueryEditText);
+        queryEditText.setText(preferences.getString(MARKET_SCHEME_QUERY_STRING_ID, DEFAULT_MARKET_SCHEME_QUERY_STRING));
+        final EditText contentIdEditText = (EditText) findViewById(R.id.contentIdEditText);
+        contentIdEditText.setText(preferences.getString(STORE_WEB_URL_CONTENT_ID, DEFAULT_STORE_WEB_URL_CONTENT_ID));
+    }
+    /**
+     * A placeholder fragment containing a simple view.
+     */
+    public static class PlaceholderFragment extends Fragment {
+        public PlaceholderFragment() {
+        }
+
+        @Override
+        public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                Bundle savedInstanceState) {
+            View rootView = inflater.inflate(R.layout.fragment_main, container,
+                    false);
+            return rootView;
+        }
+    }
+
+}
